369f1e6e7c72b6e534c5debf8b14c8ebcffcd28e
cyrillebenoit <cyrille.benoit@ymail.com>
2016-12-26 13:23:16 +0100

    Added commentaries on input.js

--- input.js
+++ input.js
@@ -9,11 +9,19 @@ const tagsToReplace = {
   'B&You': 'OPERATEUR_CONCURRENT'
 };
 module.exports = {
+  /**
+   * Returns user input before it's sent to Watson Conversation API.
+   * The rules to replace tags are set in the static array tagsToReplace.
+   * @param   text                user input
+   * @return  text                modified text
+   */
   replaceTagsUserInput: function(text) {
-    for (tag in tagsToReplace) {
-      var regex = new RegExp('\\b' + tag + '\\b', 'i')
-      while (text.search(regex) !== -1) {
-        text = text.replace(regex, tagsToReplace[tag])
+    if (Object.keys(tagsToReplace).length !== 0) {
+      for (tag in tagsToReplace) {
+        var regex = new RegExp('\\b' + tag + '\\b', 'i')
+        while (text.search(regex) !== -1) {
+          text = text.replace(regex, tagsToReplace[tag])
+        }
       }
     }
     return text;
