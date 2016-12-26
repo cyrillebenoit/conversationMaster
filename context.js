183bed151734f3bd286d17ae1abb0a4442f650db
cyrillebenoit <cyrille.benoit@ymail.com>
2016-12-26 13:04:09 +0100

    Added commentaries on context.js

--- context.js
+++ context.js
@@ -1,27 +1,17 @@
 const varsToUpdateBeforeWatson = {
-  /*nbErrors2: {
-    value: false,
-    forceIfUndefined: true,
-    function: function(answerText, context, key) {
-      //Different sets of actions depending on the answerText, can access the whole context, should only update conntext[key]
-      return context[key] * 2;
+  /*variableName: {
+    value: false OR value, //Set a particular value on every call, set to false to ignore this field.
+    forceIfUndefined: true, //If the context variable does not exist yet, tells if it should be created
+    function: function(answerText, context, key) { //Different sets of actions depending on the answerText, can access the whole context, must not update context. @return the new context[key] value.
+      return new_context[key]_value;
     }
   }*/
 };
 const varsToUpdateAfterWatson = {
-  /*nbErrors2: {
-    value: false,
-    forceIfUndefined: false,
-    function: function(answerText, context, key) {
-      //Different sets of actions depending on the answerText, can access the whole context, should only update conntext[key]
-      return context[key] + 1;
-    }
-  }*/
   noCommande: {
-    value: false,
-    forceIfUndefined: true,
-    function: function(answerText, context, key) {
-      //Different sets of actions depending on the answerText, can access the whole context, should only update conntext[key]
+    value: false, //Set a particular value on every call, set to false to ignore this field.
+    forceIfUndefined: true, //If the context variable does not exist yet, tells if it should be created
+    function: function(answerText, context, key) { //Different sets of actions depending on the answerText, can access the whole context, must not update context. @return the new context[key] value.
       var returnValue = 0;
       if (answerText.indexOf('Tout est bon pour vous ?') !== -1) {
         returnValue = Math.floor(Math.random() * 175000 + 100000); //context[key] + 1;
@@ -33,6 +23,13 @@ const varsToUpdateAfterWatson = {
   }
 };
 module.exports = {
+  /**
+   * Returns context before it's sent to Watson Conversation API.
+   * The rules to update variables are in the static array varsToUpdateBeforeWatson.
+   * @param   inMemoryContext     current context
+   * @param   messageText         user text message
+   * @return  inMemoryContext     modified context
+   */
   setContextToWatson: function(inMemoryContext, messageText) {
     if (Object.keys(varsToUpdateBeforeWatson).length !== 0) {
       for (key in varsToUpdateBeforeWatson) {
@@ -50,6 +47,11 @@ module.exports = {
     }
     return inMemoryContext;
   },
+  /**
+   * Updates context after it has been returned from Watson Conversation API.
+   * The rules to update variables are in the static array varsToUpdateAfterWatson.
+   * @param   watsonUpdate        return from Watson Conversation API which contains output, context, and all sorts of data.
+   */
   setContextAfterWatson: function(watsonUpdate) {
     if (Object.keys(varsToUpdateAfterWatson).length !== 0) {
       for (key in varsToUpdateAfterWatson) {
