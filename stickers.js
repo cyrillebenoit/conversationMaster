22f42890b13677654b293c03d7bb4e1286795abb
cyrillebenoit <cyrille.benoit@ymail.com>
2016-12-26 14:33:29 +0100

    Added commentaries on stickers.js

--- stickers.js
+++ stickers.js
@@ -1,28 +1,40 @@
 //DEFINE USED STICKER IDS
-
 //Facebook thumb stickers IDs
 const smallThumbID = 369239263222822;
 const bigThumbID = 369239343222814;
 const hugeThumbID = 369239383222810;
-
 //This array includes every answer depending on the sticker used.
 const stickers = {
+  /* sample
+  [sticker_id]: {
+    text: "Some text",
+    attachment: {
+      type: "image", //for example
+      url: 'some url'
+    }
+  },*/
   [smallThumbID]: {
     text: "Oh, c'est tout ? Je m'attendais à plus de satisfaction de votre part...",
     attachment: false
-  }, [bigThumbID]: {
+  },
+  [bigThumbID]: {
     text: "On avance mais je suis certain que vous pouvez faire mieux ! Encore un petit effort...",
     attachment: false
-  }, [hugeThumbID]: {
+  },
+  [hugeThumbID]: {
     text: "Ah ! Je vous remercie ! Vous aussi vous êtes au top !",
     attachment: false
   }
 };
-
 module.exports = {
+  /**
+   * Returns special reaction if the user sent a sticker on Messenger.
+   * The rules to react are set in the static array stickers.
+   * @param   sticker_id          sticker id
+   * @return  Object or false     answer (with(out) attachment) or false if undefined id
+   */
   reactToStickers: function(sticker_id) {
-    if (stickers[sticker_id])
-      return stickers[sticker_id];
+    if (stickers[sticker_id]) return stickers[sticker_id];
     else {
       return false;
     }
